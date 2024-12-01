360fa914010cebdfa2fa807eeaa036481958146c
Kalle <kalle.kytola@aalto.fi>
2024-12-01 22:57:20 +0200

    Initial commit.

---
+++ AoC2024Lean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `AoC2024Lean` library.
+-- Import modules here that should be built as part of the library.
+import «AoC2024Lean».Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «AoC2024Lean»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
