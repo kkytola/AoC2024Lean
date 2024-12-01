97ba3d651d75f9e8adf413c2f855f0bad65bbe87
Kalle <kalle.kytola@aalto.fi>
2024-12-02 00:12:24 +0200

    Add .lake to gitignore.

--- AoC2024Lean/AoC2024d01p1.lean
+++ AoC2024Lean/AoC2024d01p1.lean
@@ -2,7 +2,7 @@ import AoC2024Lean.AoC2024helpers
 
 def totalDist (pairs : List (Int × Int)) : Nat :=
   -- Golfs to:
-  -- `((((pairs.map (·.1)).mergeSort (· < ·)).zip ((pairs.map (·.2)).mergeSort (· < ·))).map fun p ↦ Int.natAbs (p.2 - p.1)).sum`
+  -- `((fun p ↦ Int.natAbs (p.2 - p.1)) <$> ((((·.1) <$> pairs).mergeSort (· < ·)).zip (((·.2) <$> pairs).mergeSort (· < ·)))).sum`
   let firsts := (pairs.map (·.1)).mergeSort (· < ·)
   let seconds := (pairs.map (·.2)).mergeSort (· < ·)
   let dists := (firsts.zip seconds).map fun p ↦ Int.natAbs (p.2 - p.1)
