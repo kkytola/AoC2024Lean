0378a78807d11a155d5664cc8d91e6eb34f5a241
Kalle <kalle.kytola@aalto.fi>
2024-12-02 22:39:54 +0200

    AoC2024 Day 02.

---
+++ AoC2024Lean/AoC2024d02p2.lean
@@ -0,0 +1,25 @@
+import AoC2024Lean.AoC2024helpers
+
+def safetyCheck (levels : List Int) : Bool :=
+  let diffs := (fun p ↦ p.2 - p.1) <$> (levels.drop 1).zip (levels.dropLast)
+  let decr := diffs.all (· ≤ 0)
+  let incr := diffs.all (0 ≤ ·)
+  let somegaps := diffs.all (1 ≤ Int.natAbs ·)
+  let smallgaps := diffs.all (Int.natAbs · ≤ 3)
+  (decr ∨ incr) ∧ (somegaps ∧ smallgaps)
+
+def safetyCheckWithTolerance (levels : List Int) : Bool :=
+  let sublists := (fun i ↦ levels.dropSlice i 1) <$> List.range levels.length
+  List.or <| safetyCheck <$> sublists
+
+def parseLineToList (line : String) : List Int :=
+  listOpt <| String.toInt? <$> line.splitOn " "
+
+def parseInput (input : String) : List (List Int) :=
+  parseLineToList <$> (input.splitOn "\n").filter (· ≠ "")
+
+def main : IO Unit := do
+  let input ← (IO.FS.readFile "./AoC2024Lean/input-AoC2024d02p1.txt")
+  IO.println s!"Tolerated reports : {(safetyCheckWithTolerance <$> parseInput input).count true}"
+
+--#eval main -- Tolerated reports : 4
